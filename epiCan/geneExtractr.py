6242de25fef994c353f2f66331b536d107567cd2
David <dkuo@cbio.mskcc.org>
2013-11-04 20:08:42 -0500

    first commit

---
+++ epiCan/geneExtractr.py
@@ -0,0 +1,21 @@
+import numpy as np
+import sys, os
+
+from bx.bbi.bigwig_file import BigWigFile
+### Chromsome 21
+### Extracts the start/end from the annotation file with 
+### transcript name
+
+def setUp:
+	f = open("/cbio/grlab/share/databases/encode/gm12878/rnaSeqPolACyto/wgEncodeCshlLongRnaSeqGm12878CytosolPapMinusRawSigRep1.bigWig")
+	
+
+
+def geneFeatExtractr (chrom, start, histoneFile, geneExpFile):
+	data = bw.
+
+
+
+	for each gene in genelist:
+		for each file in inputFileList:
+			extract genomic range per position
