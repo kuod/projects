83c460e11cedc5464f706a50ae3440ca015b3092
David <dkuo@cbio.mskcc.org>
2013-11-15 19:13:29 -0500

    add more later!!!

--- epiCan/predict.py
+++ epiCan/predict.py
@@ -1,3 +1,11 @@
+#Input
+#	training matrix of values including ge
+#	test vector of comparable gene expression
+#Output
+#	predicted histone track based on euclidian distance
+
 import numpy as np
-import sys, os
-import time
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
